c382018749a1e27d36d0b71fc3dd769c6fca6999
Shaofeng Zhu <shaozhu@microsoft.com>
2016-10-18 16:31:01 -0700

    Add Hello.tsx

---
+++ BrowserExecutor/src/Hello.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export class Hello extends React.Component<any, any>{
+    constructor(props: any){
+        super(props);
+    }
+
+    public render(){
+        return (<div>Hello, world</div>);
+    }
+}
+
